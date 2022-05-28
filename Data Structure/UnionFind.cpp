17676418b9bb693feaee7788be6ffaddb1ac9f68
SeongHyeok Jin <69672253+j67055@users.noreply.github.com>
2022-05-28 19:51:19 +0900

    Add files via upload

---
+++ Data Structure/UnionFind.cpp
@@ -0,0 +1,44 @@
+#include<vector>
+
+using namespace std;
+
+class UnionFind
+{
+private:
+	vector<int> p, rank;
+
+public:
+	UnionFind(int n)
+	{
+		p.assign(n, 0);
+		rank.assign(n, 0);
+		for (int i = 0; i < n; ++i)
+			p[i] = i;
+	}
+
+	int findSet(int i)
+	{
+		return (p[i] == i) ? i : p[i] = findSet(p[i]);
+	}
+
+	bool isSameSet(int i, int j)
+	{
+		return findSet(i) == findSet(j);
+	}
+
+	void unionSet(int i, int j)
+	{
+		if (isSameSet(i, j)) return;
+
+		int x = findSet(i), y = findSet(j);
+
+		if (rank[x] >= rank[y])
+		{
+			p[y] = x;
+			if (rank[x] == rank[y])
+				++rank[x];
+		}
+		else
+			p[x] = y;
+	}
+};
