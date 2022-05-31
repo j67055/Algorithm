8d2b4ec19da031bed398ff5a64cbdcbf8868ff45
SeongHyeok Jin <69672253+j67055@users.noreply.github.com>
2022-05-31 23:04:03 +0900

    Add files via upload

---
+++ Data Structure/FenwickTree/FenwickTree/FenwickTree.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class FenwickTree
+{
+private:
+	vector<int> ft;
+
+	int LSOne(int p)
+	{
+		return p & (-p);
+	}
+	void adjust(int p, int k)
+	{
+		for (; p < ft.size(); p += LSOne(p))
+			ft[p] += k;
+	}
+
+public:
+	FenwickTree(int n)
+	{
+		ft.assign(n + 1, 0);
+	}
+
+	int rsq(int p)
+	{
+		int sum = 0;
+		for (; p; p -= LSOne(p))
+			sum += ft[p];
+		return sum;
+	}
+
+	int rsq(int i, int j)
+	{
+		return rsq(j) - (i == 1 ? 0 : rsq(i - 1));
+	}
+
+	void rangeUpdate(int i, int j, int k)
+	{
+		adjust(i, k);
+		adjust(j + 1, -k);
+	}
+};
