efae43688980628a782b411d2459b7f84b2a55a1
SeongHyeok Jin <69672253+j67055@users.noreply.github.com>
2022-06-06 04:44:47 +0900

    Add files via upload

---
+++ Graph/DFS/DFS.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<stack>
+
+#define MAX 10000
+
+using namespace std;
+
+int visited[MAX];
+vector<int> adjList[MAX];
+
+void dfs(int u)
+{
+	visited[u] = 1;
+	for (int v : adjList[u])
+		if (!visited[v]) dfs(v);
+}
